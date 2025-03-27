eefbe036a1a41d81152b57965bcd3625d5a7c1e5
Stefan Andrei <32457539+canyouseethroughme@users.noreply.github.com>
2025-03-27 12:46:47 +0100

    detect function

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,50 @@ import * as handpose from '@tensorflow-models/handpose';
 import Webcam from 'react-webcam';
 
 function App() {
-  const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
+  const webcamRef = useRef<Webcam | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  const runHandPose = async () => {
-    const net = await handpose.load()
-    console.log(net);
+  const detect = async (net: handpose.HandPose) => {
+
+    if (!webcamRef.current || !canvasRef.current) return
+
+    const isWebcamReady = typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef?.current?.video?.readyState === 4
+
+    if (isWebcamReady) {
+      // get video props
+      const video = webcamRef?.current?.video
+      const videoWidth = webcamRef?.current?.video?.videoWidth
+      const videoHeight = webcamRef?.current?.video?.videoHeight
+
+      // set video height and width
+      if (webcamRef.current.video && videoHeight && videoWidth) {
+        webcamRef.current.video.height = videoHeight;
+        webcamRef.current.video.width = videoWidth;
+      }
+
+      // set canvas height and width
+      if (canvasRef.current.width && canvasRef.current.height && videoWidth && videoHeight) {
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+      }
+
+      if (video) {
+        const hand = await net.estimateHands(video);
+        console.log(hand);
+      }
+    }
   }
 
-  runHandPose()
+  const runHandPose = async () => {
+    const net = await handpose.load();
+
+    setInterval(() => {
+      detect(net);
+    },1000)
+  };
+
+  runHandPose();
+
 
   return (
     <div>
@@ -28,4 +63,5 @@ function App() {
   )
 }
 
-export default App
+
+export default App;
