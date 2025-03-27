0620f4461492ed69edd71afe257a529120095d3b
Stefan Andrei <32457539+canyouseethroughme@users.noreply.github.com>
2025-03-27 15:37:07 +0100

    fix cors errors and added fingerpose with victory and thumbs up gestures

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-// Drawing utilities from tensorflow
-// Draw functions
 import { useRef, useState } from 'react';
-// import * as tf from '@tensorflow/tfjs';
 import * as handPose from '@tensorflow-models/handpose';
+import "@tensorflow/tfjs-backend-webgl"
 import Webcam from 'react-webcam';
+import * as fingerPose from 'fingerpose';
+
+import victory from '../public/victory.png';
+import thumbs_up from '../public/thumbs-up.png';
 import { drawHand } from './utils';
 
 function App() {
   const webcamRef = useRef<Webcam | null>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const [handPoseLoaded, setHandPoseLoaded] = useState(false);
+  const [emoji, setEmoji] = useState("");
+  const images = { thumbs_up, victory }
 
   const detect = async (net: handPose.HandPose) => {
     if (!webcamRef.current || !canvasRef.current) return
-
     const isWebcamReady = typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef?.current?.video?.readyState === 4
 
     if (isWebcamReady) {
@@ -38,6 +41,24 @@ function App() {
         const hand = await net.estimateHands(video);
         const ctx = canvasRef.current.getContext('2d');
 
+        if (hand.length > 0) {
+          const GE = new fingerPose.GestureEstimator([
+            fingerPose.Gestures.VictoryGesture,
+            fingerPose.Gestures.ThumbsUpGesture
+          ])
+
+          const keyPoints3D = hand[0].landmarks
+          const gesture = GE.estimate(keyPoints3D as [], 8)
+          
+          if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+            const confidence = gesture.gestures.map((prediction) => prediction.score)
+            const maxConfidence = confidence.indexOf(Math.max.apply(null, confidence))
+            setEmoji(gesture.gestures[maxConfidence].name)
+          } else {
+            setEmoji("")
+          }
+        }
+
         if (ctx) {
           drawHand(hand, ctx)
         }
@@ -49,11 +70,11 @@ function App() {
     const net = await handPose.load();
     if (net) {
       setHandPoseLoaded(true)
-    }
+    } 
 
     setInterval(() => {
       detect(net);
-    }, 300)
+    }, 2000)
   };
 
   runHandPose();
@@ -61,9 +82,12 @@ function App() {
   return (
     <div>
       {handPoseLoaded && <h1>The model has been loaded. Now WAVE!</h1>}
+      {handPoseLoaded && <h2>You can also do a finger pose, like thumbs up! Or victory sign!</h2>}
 
       <Webcam width={1920} height={1080} ref={webcamRef} className='webCamStyle' />
       <canvas ref={canvasRef} className='webCamStyle' />
+
+      {emoji !== "" && <img className='fingerPose' src={images[emoji as keyof typeof images]} alt="emoji" />}
     </div>
   )
 }
