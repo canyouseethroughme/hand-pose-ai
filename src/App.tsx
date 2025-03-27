0634781729e6f274a056fc6cfea93c3cf7eeb497
Stefan Andrei <32457539+canyouseethroughme@users.noreply.github.com>
2025-03-27 11:02:16 +0100

    defined references

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+// Define references
+// Load handpose
+// Detect function
+// Drawing utilities from tensorflow
+// Draw functions
+import { useRef } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as handpose from '@tensorflow-models/handpose';
+import Webcam from 'react-webcam';
+
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  
+
   return (
     <div>
-      this is it
+      <Webcam ref={webcamRef} className='webCamStyle' />
+
+      <canvas ref={canvasRef} className='webCamStyle' />
     </div>
   )
 }
